a5c539fca14f33164fde53a8a027f3b0879e6bdc
Anagha-14-coder <anaghakumar14@gmail.com>
2024-08-03 22:40:20 +0530

    Add files via upload

---
+++ skip_odd_and_exit_when_multiplication_of_5.py
@@ -0,0 +1,20 @@
+def skip_and_multiply():
+    start = 1
+    end = 10
+    
+    for current_number in range(start, end ):
+        
+        if current_number % 5 == 0:
+             break
+        if current_number % 2 == 0:  # Skip odd numbers
+            
+         print(current_number)
+        
+         
+skip_and_multiply()
+            
+        
+       
+        
+        
+
